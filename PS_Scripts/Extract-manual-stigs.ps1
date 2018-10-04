9dc4a1860ffce96e752e06dd13faee90e69a4d0b
anthropolite <anthropolite10@gmail.com>
2018-10-04 00:05:09 -0400

    added stig-extraction folder and worked on extract manual stigs.  Need first extract all stigs then remove them then find new list.  current method is removing files before they are extradcted.  Also need to work in capturing of eerrors as some stigs write to the same directory/filename

--- PS_Scripts/Extract-manual-stigs.ps1
+++ PS_Scripts/Extract-manual-stigs.ps1
@@ -1,13 +1,13 @@
 ﻿# Set path to Script Files
 
 $global:path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$global:stig_extract_path = "$path\..\STIG-extraction"
 $global:stig_path = "$path\..\STIGS"
 $stigarchive = $global:path
 
 # get location of the STIG Archive
 
-while (-not (test-path $stigArchive -PathType leaf) -and ($stigArchive -notlike "*.zip"))
-{
+while (-not (test-path $stigArchive -PathType leaf) -and ($stigArchive -notlike "*.zip")){
     $StigArchive = Read-host -prompt "Input the full path to the stig archive (path & filename)"
     
     if (-not (test-path $stigArchive -PathType leaf)) 
@@ -21,26 +21,43 @@ while (-not (test-path $stigArchive -PathType leaf) -and ($stigArchive -notlike 
 
 # extension filter to a file extension that exists
 # inside your ZIP file
-$Filter = '*_Manual-xccdf.xml'
+$zipfiles = $stigarchive
+$StigFilter = '*_Manual-xccdf.xml'
+$zipfilter = '*.zip'
 
-# load ZIP methods
-Add-Type -AssemblyName System.IO.Compression.FileSystem
+While ($zipfiles -ne $null){
 
-# open ZIP archive for reading
-$zip = [System.IO.Compression.ZipFile]::OpenRead($StigArchive)
-
-# find all files in ZIP that match the filter (i.e. file extension)
-$files=$zip.Entries | Where-Object { $_.FullName -like $Filter }
-
-  ForEach($file in $files){ 
-    # extract the selected items from the ZIP archive
-    # and copy them to the out folder
-
-    $FileName = $_.Name
-    
-    write-host "Extracting $filename to $stig_path"
-    [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$stig_path\$FileName", $true)
+    foreach ($archive in $zipfiles){
+        
+        # extractiarchiveZIP archive
+        if ($archive.fullname -eq $null){
+            write-host "Extracting $archive to $stig_extract_path"
+            expand-Archive -literalpath $archive -DestinationPath $global:stig_extract_path
+            remove-item -LiteralPath $archive
+        }else{
+            $zip=$archive.fullname
+            write-host "Extracting $archive to $($archive.directoryname)"
+            expand-Archive -literalpath $zip -destinationpath $archive.directoryname 
+            remove-item -LiteralPath $archive
+        }
     }
 
-# close ZIP file
-$zip.Dispose()
+    # find all files in ZIP that match the filter (i.e. file extension)
+    $zipfiles = Get-ChildItem -path $global:stig_extract_path -Include *.zip -Recurse
+    
+}
+
+<#    iles){ 
+            # extract the selected items from the ZIP archive
+            # and copy them to the out folder
+            #$subzip = [System.IO.Compression.ZipFile]::OpenRead($zip)
+        
+            $FileName = $zip.Name
+    
+            write-host "Extracting zip file $filename to $stig_path"
+            [System.IO.Compression.ZipFileExtensions]::ExtractToFile($arch, "$stig_path\$FileName", $true)
+        }
+    
+        # close ZIP file
+        $zip.Dispose()
+    } #>
